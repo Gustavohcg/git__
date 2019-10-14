19d5d65f3773ddd6819ef6cb6ea4431bd4ff0460
Gustavo Guerreiro <gustavo.carvalhal.guerreiro@gmail.com>
2019-10-14 17:26:45 -0300

    Teste 2

--- gitt/Program.cs
+++ gitt/Program.cs
@@ -4,6 +4,7 @@ namespace gitt {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Teste 1");
+            Console.WriteLine("Teste 2");
         }
     }
 }
